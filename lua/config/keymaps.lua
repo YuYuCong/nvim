eaf61261677aac741d81972926ae71abab7055db
cong.yu <windmillyucong@163.com>
2024-08-21 23:20:34 +0800

    keymap and opt

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local unmap = vim.keymap.del
+
+map("n", "<S-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+-- 关闭相对行号
+opt.relativenumber = false
+
+-- bigfix阈值大小
+vim.g.bigfile_size = 1024 * 1024 * 50 -- 50 MB
